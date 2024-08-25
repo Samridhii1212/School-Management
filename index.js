01ab35223d24e2b83606703b7887502a8bd724fd
Samridhii1212 <115480641+Samridhii1212@users.noreply.github.com>
2024-08-25 14:27:04 +0530

    done

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const schoolRoutes = require('./routes/schoolRoutes');
+
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -17,6 +20,8 @@ db.connect(err => {
   if (err) throw err;
   console.log('MySQL Connected...');
 });
+
+app.use('/api', schoolRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
